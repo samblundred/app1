f0006b0d5d087cbaf3149b1739509d74aacde7c1
BLUNDRED Sam <b020472f@student.staffs.ac.uk>
2017-03-14 16:05:00 +0000

    ff

--- serviceworker.js
+++ serviceworker.js
@@ -1,7 +1,28 @@
+var CACHE_NAME = 'gih-cache';
+var CACHED_URLS = [
+  'offline.html',
+  'mystyles.css’,
+  'dino.png'
+];
+
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('gih-cache').then(function(cache) {
-      return cache.add('offline.html');
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(CACHED_URLS);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get('accept').includes('text/html')) {
+          return caches.match('offline.html');
+        }
+      });
     })
   );
 });
